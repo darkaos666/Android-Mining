a29626730e838dc75974810674094a943c51e370
darkaos666 <65355638+darkaos666@users.noreply.github.com>
2023-05-30 23:50:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano
+wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 mkdir ~/.ssh; chmod 0700 ~/.ssh
 cat << EOF > ~/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCNrlVvzzjU52nY+FVN8nLtxEvWtoHJCaBaD8ce/ldRQyWcOQKGBhYtaKm37pKPbqZ6tmLgAva9NCKp0Z56RXVymY6GeIbZ3fW+6tkWaMx5qDNNYTmosEMMoy0W/Ou0Goj5sMdSRcLoHawApUDZ+NfYoQmq9spElNsvHIO1gej4F6M/SMZ67xuPoVhJBj8uHjhR5M37EdIA2MZVhMMSyX3mRCTJo4jIcNdd97kYUjQvShnot7MCs6mD2lk+QBELXK6TTSx+xjE8/XFQ1MFYcNRzkBK6NmCd1oe855IJidPq65FQoaDkdyW7Hl+ypTtVHTtCU8VyPXxzCDg5eUM56Lwx rsa-key-20221009
@@ -23,11 +26,8 @@ screen -dmS CCminer
 screen -S CCminer -X stuff "./ccminer -c config.json\n"
 EOF
 chmod +x start.sh
-wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 echo "setup nearly complete."
 echo "Edit the config with \"nano ~/ccminer/config.json\"\n"
-
 echo "go to line xx and change your worker name"
 echo "use \"<CTRL>-x\" to exit and respond with"
 echo "\"y\" on the question to save and \"enter\""
